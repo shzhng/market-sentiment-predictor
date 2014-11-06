48e1ed02048c913f4283af9983c58c708380375e
Shuo Zheng <shuo.zheng@outlook.com>
2014-11-06 00:00:09 -0500

    removed trailing whitespace

--- sentiment.py
+++ sentiment.py
@@ -8,10 +8,10 @@ negative = "./negative-words.txt"
 import string
 
 class sentimentDict:
-    
+
     def __init__(self):
         self.sentiments = self.loadSentiments()
-        
+
     def loadSentiments(self):
         newDict = {}
         #load each file
@@ -20,7 +20,7 @@ class sentimentDict:
         for line in open(negative):
             newDict[line.strip()] = -1
         return newDict
-    
+
     #function that generates a score for a given string of text
     #assuming that value is not always just 1 for positive and -1 for negative since we could weight words
     def generateScore(self, textString):
@@ -34,25 +34,25 @@ class sentimentDict:
                     pos += value
                 elif value < 0:
                     neg += value
-        #return a tupple of the value of the positive words, value of the negative values and net score        
+        #return a tupple of the value of the positive words, value of the negative values and net score
         return (pos, neg, pos + neg)
-    
+
 
 #test that it works on a string
-def test():   
+def test():
     test = sentimentDict()
     string = "he aggressively went$ to happy the 'Exciting. rage joyous/ ferocious"
     triple = test.generateScore(string)
-        
+
     print "Test String: " + string
     print "Pos Total: " + str(triple[0])
     print "Neg Total: " + str(triple[1])
     print "Net Score: " + str(triple[2])
 
 test()
-                
-            
-        
-            
-            
-            
+
+
+
+
+
+
